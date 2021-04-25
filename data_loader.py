dd547270b4ac31fd9ffd8d0197e8017c8950fbb0
Ajaykumar Kannan <kannan.ajay@gmail.com>
2021-04-25 03:25:04 -0400

    Got a plotter function working for some Canadian states

--- data_loader.py
+++ data_loader.py
@@ -1,10 +1,15 @@
 #!/usr/bin/env python3
 import csv
 
-def load_database() -> dict:
-	data_list = ['Canada']
-	database = {}
+g_database= {}
+
+def load_database(data_list = ['Canada']) -> dict:
+	database = g_database
 	for country in data_list:
+		if country not in database:
+			print("Reloading the data for " + country)
+		else:
+			return database
 		country_entry = {}
 		with open('data/' + country.lower() + '.csv') as csvfile:
 			csvReader = csv.reader(csvfile, delimiter=',')
@@ -24,11 +29,31 @@ def load_database() -> dict:
 				except ValueError:
 					print("TODO: Issues")
 		database[country] = country_entry
-	
+
 	return database
 
+def compute_tax_helper(income : float, data : list):
+	taxes = 0
+	remaining_income = income
+	for (income_range, rate) in reversed(data):
+		cur_taxable_income = max(0, remaining_income - income_range)
+		remaining_income -= cur_taxable_income
+		taxes += cur_taxable_income * rate / 100.0
+
+	return taxes
+
+def compute_tax(country: str, state: str, income : float, year : int = 2021) -> float:
+	db = load_database([country])
+	country_data = db[country]
+	state_data = country_data[year][state]
+	federal_data = country_data[year]['Federal']
+	state_taxes = compute_tax_helper(income, state_data)
+	federal_taxes = compute_tax_helper(income, federal_data)
+
+	return (state_taxes, federal_taxes)
+
 def main():
-	print(load_database())
+	print(compute_tax('Canada', 'Ontario', 100000))
 
 
 if __name__ == '__main__':
