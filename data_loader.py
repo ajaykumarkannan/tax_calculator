7ce4634b2565a11ef6ed1a5bb6d07ad682ca41ad
Ajaykumar Kannan <kannan.ajay@gmail.com>
2021-04-28 05:09:54 +0000

    Added support for basic personal income

--- data_loader.py
+++ data_loader.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+# vim: set noexpandtab shiftwidth=2 tabstop=2 :
 import csv
 from matplotlib import pyplot as plt
 import matplotlib
@@ -7,6 +8,7 @@ g_database = {}
 g_EI_database = {}
 g_QB_EI_database = {}
 g_CPP_database = {}
+g_BPA_database = {}
 
 state_codes = {
 		"AB" : "Alberta",
@@ -53,22 +55,11 @@ def load_database(data_list = ['Canada']) -> dict:
 
 	return database
 
-def compute_tax_helper(income : float, data : list):
-	taxes = 0
-	remaining_income = income
-	for (income_range, rate) in reversed(data):
-		cur_taxable_income = max(0, remaining_income - income_range)
-		remaining_income -= cur_taxable_income
-		taxes += cur_taxable_income * rate / 100.0
-
-	return taxes
-
 def loadEI(fileName, database) -> None:
 	with open(fileName) as csvfile:
 		csvReader = csv.reader(csvfile, delimiter=',')
 		print("Loading", fileName)
 		for row in csvReader:
-			print(row)
 			try:
 				if csvReader.line_num == 1:
 					continue
@@ -92,13 +83,12 @@ def compute_EI(country: str, state: str, income: float, year: int = 2021) -> flo
 	EI = 0.0
 	if country == "Canada":
 		if state == "Quebec" or state == "QB":
-			print("Loading Quebec")
 			database = g_QB_EI_database
 		else:
 			database = g_EI_database
 		rate = database[year][1]
 		max_val = database[year][2]
-		EI = min(income * rate, max_val)
+		EI = min(income * rate / 100.0, max_val)
 	return EI
 
 def loadCPP(fileName, database):
@@ -106,7 +96,6 @@ def loadCPP(fileName, database):
 		csvReader = csv.reader(csvfile, delimiter=',')
 		print("Loading", fileName)
 		for row in csvReader:
-			print(row)
 			try:
 				if csvReader.line_num == 1:
 					continue
@@ -130,8 +119,6 @@ def compute_CPP(country: str, state: str, income: float, year: int = 2021) -> fl
 	CPP = 0.0
 	if country == "Canada":
 		database = g_CPP_database
-		rate = database[year][1]
-		max_val = database[year][2]
 		max_annual_earnings = database[year][0]
 		exception_amount = database[year][1]
 		max_cont_earnings = database[year][2]
@@ -139,11 +126,60 @@ def compute_CPP(country: str, state: str, income: float, year: int = 2021) -> fl
 		max_contribution = database[year][4]
 		max_self_contribution = database[year][5]
 		CPP_income = max(0, income - exception_amount)
-		CPP = min(max_contribution, CPP_income * rate)
+		CPP = min(max_contribution, CPP_income * rate / 100.0)
 	return CPP
 
-def compute_Canada_BPA(country: str, state: str, income: float, year: int = 2021) -> float:
-	return 0
+def loadBPA(fileName, database):
+	with open(fileName) as csvfile:
+		csvReader = csv.reader(csvfile, delimiter=',')
+		print("Loading", fileName)
+		for row in csvReader:
+			try:
+				if csvReader.line_num == 1:
+					continue
+				year = int(row[0])
+				state = row[1].strip()
+				income = float(row[2])
+				rate = float(row[3])
+				if year not in database:
+					database[year] = {}
+				database[year][state] = (income, rate)
+
+			except ValueError:
+				print("TODO: Issues")
+	return
+
+def compute_BPA(country: str, state: str, income: float, year: int = 2021) -> float:
+	BPA_database = g_BPA_database
+	if not BPA_database:
+		print("Reloading BPA data")
+		loadBPA('data/canadaBPA.csv', BPA_database)
+	BPA_deduction = 0.0
+	if country == "Canada":
+		database = g_BPA_database
+		if year not in database: 
+			print("Couldn't find the right yera")
+			return 0
+		if state in database[year]:
+			state_income = database[year][state][0]
+			state_rate = database[year][state][1]
+			BPA_deduction += min(income, state_income) * state_rate / 100.0
+		else:
+			print("Found an issue.")
+			return 0
+		federal_income = database[year]["Federal"][0]
+		federal_rate = database[year]["Federal"][1]
+		BPA_deduction += min(income, federal_income) * federal_rate / 100.0
+	return BPA_deduction
+
+def compute_tax_helper(income : float, data : list):
+	taxes = 0
+	remaining_income = income
+	for (income_range, rate) in reversed(data):
+		cur_taxable_income = max(0, remaining_income - income_range)
+		remaining_income -= cur_taxable_income
+		taxes += cur_taxable_income * rate / 100.0
+	return taxes
 
 def compute_tax(country: str, state: str, income : float, year : int = 2021) -> float:
 	db = load_database([country])
@@ -156,6 +192,7 @@ def compute_tax(country: str, state: str, income : float, year : int = 2021) -> 
 	total_taxes = compute_tax_helper(income, state_data)
 	total_taxes += compute_EI(country, state, income, year)
 	total_taxes += compute_CPP(country, state, income, year)
+	total_taxes -= compute_BPA(country, state, income, year)
 
 	return total_taxes
 
